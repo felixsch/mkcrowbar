7055de296c585acd161b04a7036efff44251e26e
Felix Schnizlein <felix@schnizle.in>
2016-03-30 15:32:38 +0200

    Formated missing paths.py

--- src/mkcrowbar/paths.py
+++ src/mkcrowbar/paths.py
@@ -1,20 +1,18 @@
 
 
 def barclamps(git_install=False):
-   if git_install:
-      return '/root/crowbar/barclamps'
-   return '/opt/dell/barclamps'
+    if git_install:
+        return '/root/crowbar/barclamps'
+    return '/opt/dell/barclamps'
+
 
 def crowbar_json(name, git_install=False):
-   path = '/etc/crowbar/'
-   if git_install:
-      path = '/root/crowbar/'
+    path = '/etc/crowbar/'
+    if git_install:
+        path = '/root/crowbar/'
+    return path + name + '.json'
 
-   return path + name + '.json'
 
 def crowbar_chef_templates(name):
-   path = '/opt/dell/chef/data_bags/crowbar/bc-template-'
-   
-   return path + name + '.json'
-
-
+    path = '/opt/dell/chef/data_bags/crowbar/bc-template-'
+    return path + name + '.json'
